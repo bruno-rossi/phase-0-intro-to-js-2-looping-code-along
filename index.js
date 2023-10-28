021fcfb23413b4bb4e0cd1a4a8535ebd28cd9b7b
Bruno Rossi <brunogabrielrossi@gmail.com>
2023-10-28 17:19:53 -0400

    Submitting Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) { 
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+// const tyCards = {
+//     guestName: ["Charlie", "Samip", "Ali"],
+//     eventName: "birthday"
+// }
+
+// function writeCards(friend, eventName) {
+//     for (let counter = 0; counter < friend.length; counter++) {
+//         console.log(`"Thank you, ${friend[counter]}, for the wonderful ${eventName} gift!"`);
+//         debugger;
+//     }
+// }
+
+// writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+
+function writeCards(friends, eventName) {
+    let messages = []
+    for (let i = 0; i < friends.length; i++) {
+        messages.push(`Thank you, ${friends[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+
+countDown();
